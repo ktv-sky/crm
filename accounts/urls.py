629b28bbd74498c50a80aff0c5474f8f531d7514
KTV <ktv@MacBook-Pro-KTV.local>
2020-12-24 14:39:49 +0300

    Initial commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer, name='customer'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product, Customer, Order, Tag
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
 
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
 
-def products(request):
-    return render(request, 'accounts/products.html', {
-        'products': Product.objects.all()
+    return render(request, 'accounts/dashboard.html', {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
     })
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {
+        'products': products
+    })
+
+
+def customer(request, pk):
+    customer = get_object_or_404(Customer, id=pk)
+    orders = customer.order_set.all()
+
+    return render(request, 'accounts/customer.html', {
+        'customer': customer,
+        'orders': orders,
+    })
