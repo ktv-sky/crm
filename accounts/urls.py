5571db2009c04e77a110f638aeacf82596056fa5
KTV <helicooper7@gmail.com>
2021-01-11 16:30:01 +0300

    Add password reset email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -22,4 +23,26 @@ urlpatterns = [
         name='update_order'),
     path('delete_order/<int:order_id>/', views.delete_order,
         name='delete_order'),
+
+    path('reset_password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html'
+        ),
+        name='reset_password'),
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_sent.html'
+        ),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_form.html'
+        ),
+        name='password_reset_confirm'),
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name='password_reset_complete'),
+
 ]
