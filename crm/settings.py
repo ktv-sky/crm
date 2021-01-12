8d67d44e81cb8a33e84edadd9652cc36b31c191f
KTV <helicooper7@gmail.com>
2021-01-12 13:01:32 +0300

    Setup PostgreSQL

--- crm/settings.py
+++ crm/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm_db',
+        'USER': 'crm_user',
+        'PASSWORD': 'devpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+
     }
 }
 
